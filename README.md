e1c072925c71f5fcd3498ea1f3dc8f7824d0e37c
Celeo <mattboulanger@fastmail.com>
2026-09-21 22:52:02 -0700

    progress

---
+++ README.md
@@ -0,0 +1,4 @@
+# simple_llm_tui
+
+An extremely simple LLM harness for learning. Not intended for actual practical use; use [crush](https://github.com/charmbracelet/crush),
+[aider](https://github.com/Aider-AI/aider), or [pi](https://github.com/earendil-works/pi).
---
+++ src/simple_llm_tui/__init__.py
@@ -0,0 +1,80 @@
+import asyncio
+
+import httpx
+from pydantic import BaseModel
+from rich.console import Console
+from rich.prompt import Prompt
+
+URL = "http://localhost:11434/v1/chat/completions"
+MODEL = "qwen3:8b"
+
+
+class ChatMessage(BaseModel):
+    role: str
+    content: str
+
+
+class ChatRequest(BaseModel):
+    model: str
+    messages: list[ChatMessage]
+
+
+class ChatMessageWrapper(BaseModel):
+    index: int
+    message: ChatMessage
+    finish_reason: str
+
+
+class Usage(BaseModel):
+    prompt_tokens: int
+    completion_tokens: int
+    total_tokens: int
+
+
+class ChatResponse(BaseModel):
+    id: str
+    object: str
+    created: int
+    model: str
+    system_fingerprint: str | None = None
+    choices: list[ChatMessageWrapper]
+    usage: Usage
+
+
+async def call_llm(input: ChatRequest) -> ChatResponse:
+    async with httpx.AsyncClient() as client:
+        response = await client.post(URL, json=input.model_dump())
+        if response.status_code != 200:
+            raise ValueError(
+                f"Error response code {response.status_code} from Ollama: {response.text}"
+            )
+        return ChatResponse.model_validate(response.json())
+
+
+def wrap_user(content: str) -> ChatMessage:
+    return ChatMessage(role="user", content=content)
+
+
+def wrap_input(previous: list, next: ChatMessage) -> ChatRequest:
+    return ChatRequest(model=MODEL, messages=[*previous, next])
+
+
+async def main() -> None:
+    console = Console()
+    messages = []
+    while True:
+        message = Prompt.ask("[green]Input[/green]")
+        if not message:
+            console.print("\n[cyan]Bye! :wave:[/cyan]")
+            break
+        input = wrap_user(message)  # messages)
+        input = wrap_input([], input)
+        console.print_json(input.model_dump_json())
+        response = await call_llm(input)
+        console.print_json(response.model_dump_json())
+        messages.append(input)
+        # messages.extend([input, *response.choices])
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
